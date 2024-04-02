38dc7a2bbbccbe71c8664f559b98beda1d80bb08
dnzlucas <lucasdefenix@gmail.com>
2024-04-02 16:59:32 -0300

    Feat: criação da página Members

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
+<<<<<<< Updated upstream
   <RouterView/>
+=======
+  <RouterView />
+>>>>>>> Stashed changes
 </template>
 
 <style scoped>
