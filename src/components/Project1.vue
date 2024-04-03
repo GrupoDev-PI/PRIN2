d6f0125b0e7b117cad9e82b6eea3b60db6f7c552
LuizBlume <luiz.fernando.251107@gmail.com>
2024-04-03 15:23:14 -0300

    Feat: Atualização na página de Project #3.

---
+++ src/components/Project1.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="project">
+    <div class="container">
+      <div class="project-content">
+        <div class="project-image">
+          <img src="../assets/img/project1.png" alt="Project 1" />
+        </div>
+        <div class="project-info">
+          <h2>Projeto 1</h2>
+          <p>
+            Trabalho de Desenvolvimento Web I. Consistia em escolher uma empresa da região e fazer
+            um site front-end. Utilizamos tecnologias Back-end e API, para pegar os filmes em tempo
+            real.
+          </p>
+          <p>
+            <b
+              >Instalar a extensão
+              <a
+                href="https://chromewebstore.google.com/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf?pli=1"
+                >Allow Colors</a
+              ></b
+            >
+          </p>
+          <a href="https://trabalho-gnc.vercel.app/" class="btn">Ver Projeto</a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<style scoped>
+@import '../assets/scss/components/_project1.scss';
+</style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,7 +1,9 @@
 <script setup>
 import Header from '../components/Header.vue'
+import Project1 from '../components/Project1.vue'
 </script>
 
 <template>
     <Header />
+    <Project1 />
 </template>
