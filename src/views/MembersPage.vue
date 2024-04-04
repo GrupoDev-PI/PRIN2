3cfc8f3401421e5a9916c9e7e82a5eda6cee5b5d
dnzLucas <lucasdefenix@gmail.com>
2024-04-04 16:40:45 -0300

    style: alteração nos estilos dos cards.

--- src/views/MembersPage.vue
+++ src/views/MembersPage.vue
@@ -1,12 +1,12 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted } from 'vue'
 import axios from 'axios'
 
-const dadosMembros = ref([]);
+const dadosMembros = ref([])
 
 onMounted(async () => {
-  const { data } = await axios.get('http://localhost:3000/membros');
-  console.log(data);
+  const { data } = await axios.get('http://localhost:3000/membros')
+  console.log(data)
   dadosMembros.value = data
 })
 </script>
@@ -15,12 +15,13 @@ onMounted(async () => {
   <h1>Member's Page</h1>
   <div class="profiles-container">
     <div v-for="(membro, membroIndex) in dadosMembros" class="profile-content">
-      <div><span class="dot"></span></div>
-      <div>
-        <h2>Nome do cara: {{ membro.nome }}</h2>
-        <p>insta: {{ membro.instagram }}</p>
-        <p>github: {{ membro.github }}</p>
-    </div>
+      <div><img :src="membro.foto" alt="" /></div>
+      <div class="socialMedia" >
+        <h2>{{ membro.nome }}</h2>
+        <p>{{ membro.instagram }}</p>
+        <p>{{ membro.github }}</p>
+        <button>hdsjifbsdf</button>
+      </div>
     </div>
   </div>
 </template>
@@ -38,9 +39,8 @@ h1 {
 
 .profile-content {
   width: 800px;
-  height: 400px;
+  height: 350px;
   margin: 10px;
-  border: 1px solid #ccc;
   border-radius: 8px;
   display: flex;
   flex-direction: row;
@@ -48,17 +48,31 @@ h1 {
   align-items: flex-start;
   justify-content: space-between;
   padding: 30px;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+  transition: 0.2s ease-in-out;
 }
 
-.profile-content div{
-    height: 100%;
-    width: 100%;
-    text-align: center;
+.profile-content:hover {
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
 }
 
+.profile-content div {
+  height: 100%;
+  width: 100%;
+  text-align: center;
+}
 
+button {
+  background-color: #4caf50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
 
-.dot {
+.profile-content img {
   height: 13vw;
   width: 13vw;
   background-color: #bbb;
